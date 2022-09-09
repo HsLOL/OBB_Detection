512a305c2cc2ab429f295a05981e884f4cbb12fa
jianhonghan <751401234@qq. om>
2022-09-09 12:59:24 +0800

    add hsv augment

--- python/jdet/data/transforms.py
+++ python/jdet/data/transforms.py
@@ -23,9 +23,14 @@ class Compose:
             self.transforms.append(transform)
 
     def __call__(self, image, target=None):
+        # ----AugmentHSV
         for t in self.transforms:
-            image, target = t(image, target)
-        
+            if hasattr(t, 'name'):
+                image = t(image)
+            else:
+                image, target = t(image, target)
+        # -------------------
+
         return image, target
 
 @TRANSFORMS.register_module()
@@ -600,6 +605,37 @@ class YoloAugmentHSV:
 
         img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))).astype(dtype)
         cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
+
+#------add HSV Augment ---20220909
+@TRANSFORMS.register_module()
+class AugmentHSV:
+    def __init__(self, hgain=0.5, sgain=0.5, vgain=0.5):
+        self.hgain=hgain
+        self.sgain=sgain
+        self.vgain=vgain
+        self.name = 'HSVAug'
+
+    def __call__(self, img):
+        #pil to cv2
+        img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
+
+        r = np.random.uniform(-1, 1, 3) * [self.hgain, self.sgain, self.vgain] + 1  # random gains
+        hue, sat, val = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
+        dtype = img.dtype  # uint8
+
+        x = np.arange(0, 256, dtype=np.int16)
+        lut_hue = ((x * r[0]) % 180).astype(dtype)
+        lut_sat = np.clip(x * r[1], 0, 255).astype(dtype)
+        lut_val = np.clip(x * r[2], 0, 255).astype(dtype)
+
+        img_hsv = cv2.merge((cv2.LUT(hue, lut_hue), cv2.LUT(sat, lut_sat), cv2.LUT(val, lut_val))).astype(dtype)
+        cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
+
+        #cv2 to pil
+        img = Image.fromarray(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+        return img
+#----------------------
+
 
 
 @TRANSFORMS.register_module()
